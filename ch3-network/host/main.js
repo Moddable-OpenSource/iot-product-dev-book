8f1576c4d7fa2e55269b07d56ccd6a6472e05710
Peter Hoddie <peter@moddable.tech>
2020-11-13 18:04:20 -0800

    Wi-Fi credentials can be in either the host or the mod. If in both, mod takes priority.

--- ch3-network/host/main.js
+++ ch3-network/host/main.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2016-2020 Moddable Tech, Inc.
  *
  *   This file is part of the Moddable SDK.
- * 
+ *
  *   This work is licensed under the
  *       Creative Commons Attribution 4.0 International License.
  *   To view a copy of this license, visit
@@ -15,6 +15,7 @@
 import LoadMod from "loadmod";
 import WiFi from "wifi";
 import Net from "net";
+import config from "mc/config";
 
 export default function () {
 	if (!LoadMod.has("check") || !LoadMod.has("example"))
@@ -22,37 +23,36 @@ export default function () {
 
 	(LoadMod.load("check"))();
 
+	let credentials;
 	if (LoadMod.has("mod/config")) {
-		const config = LoadMod.load("mod/config");
-		if (config.ssid) {
-			trace(`Wi-Fi trying to connect to "${config.ssid}"\n`);
-
-			WiFi.mode = 1;
-
-			let monitor = new WiFi({ssid: config.ssid, password: config.password}, function(msg, code) {
-			   switch (msg) {
-				   case WiFi.gotIP:
-						trace(`IP address ${Net.get("IP")}\n`);
-						monitor.close();
-
-						LoadMod.load("example");
-						break;
-
-					case WiFi.connected:
-						trace(`Wi-Fi connected to "${Net.get("SSID")}"\n`);
-						break;
-
-					case WiFi.disconnected:
-						trace((-1 === code) ? "Wi-Fi password rejected\n" : "Wi-Fi disconnected\n");
-						break;
-				}
-			});
-			return;
-		} else {
-			throw new Error(`This example requires Wi-Fi. When executing mcrun specify ssid and password.\n`);
-			return;
-		}
+		credentials = LoadMod.load("mod/config");
+		if (!credentials.ssid)
+			credentials = undefined;
 	}
+	credentials ??= config;
+	if (!credentials.ssid)
+		return trace("This example requires Wi-Fi. When executing mcrun specify ssid and password.\n");
 
-	LoadMod.load("example");
+	trace(`Wi-Fi trying to connect to "${credentials.ssid}"\n`);
+
+	WiFi.mode = 1;
+
+	const monitor = new WiFi({ssid: credentials.ssid, password: credentials.password}, function(msg, code) {
+	   switch (msg) {
+		   case WiFi.gotIP:
+				trace(`IP address ${Net.get("IP")}\n`);
+				monitor.close();
+
+				LoadMod.load("example");
+				break;
+
+			case WiFi.connected:
+				trace(`Wi-Fi connected to "${Net.get("SSID")}"\n`);
+				break;
+
+			case WiFi.disconnected:
+				trace((-1 === code) ? "Wi-Fi password rejected\n" : "Wi-Fi disconnected\n");
+				break;
+		}
+	});
 }
